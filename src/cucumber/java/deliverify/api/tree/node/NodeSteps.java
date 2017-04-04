3eae9feec5fc94a2de76b758e9c60a8bade83671
João Loureiro <joao.a.g.loureiro@gmail.com>
2017-04-04 15:43:31 +0100

    Step definitions for the node's new scenarios

--- src/cucumber/java/deliverify/api/tree/node/NodeSteps.java
+++ src/cucumber/java/deliverify/api/tree/node/NodeSteps.java
@@ -1,5 +1,7 @@
 package deliverify.steps.api.tree;
 
+import java.util.ArrayList;
+
 import cucumber.api.java.en.*;
 
 import static org.junit.Assert.*;
@@ -11,9 +13,12 @@ public class NodeSteps {
   private int x;
   private int y;
 
-  private int cost;
+  private Node<int[]> node;
 
-  private Node node;
+  private int childX;
+  private int childY;
+
+  private Node<int[]> childNode;
 
   @Given("^I have to create a node in x=(\\d+) and y=(\\d+)$")
   public void iHaveToCreateANodeInXAndY(int x, int y) throws Throwable {
@@ -22,25 +27,49 @@ public class NodeSteps {
     this.y = y;
   }
 
-  @Given("^it has a cost if (\\d+)$")
-  public void itHasACostIf(int cost) throws Throwable {
-
-    this.cost = cost;
-  }
-
   @When("^I create the node$")
   public void iCreateTheNode() throws Throwable {
 
-    this.node = new Node(x, y);
-    this.node.setCost(this.cost);
+    this.node = new Node<int[]>(new int[] {this.x, this.y});
   }
 
   @Then("^all the information it contais must be correct$")
   public void allTheInformationItContaisMustBeCorrect() throws Throwable {
 
-    assertEquals(this.x, this.node.getX());
-    assertEquals(this.y, this.node.getY());
-    assertEquals(this.cost, this.node.getCost());
+    int x = this.node.getData()[0];
+    int y = this.node.getData()[1];
+
+    assertEquals(this.x, x);
+    assertEquals(this.y, y);
+  }
+
+  @Given("^that node has a child at x=(\\d+) and y=(\\d+)$")
+  public void thatNodeHasAChildAtXAndY(int childX, int childY) throws Throwable {
+
+    this.childX = childX;
+    this.childY = childY;
+  }
+
+  @When("^I create the child of that node$")
+  public void iCreateTheChildOfThatNode() throws Throwable {
+
+    this.childNode = this.node.addChild(new int[] {this.childX, this.childY});
+  }
+
+  @Then("^the node must have a child with the correct data$")
+  public void theNodeMustHaveAChildWithTheCorrectData() throws Throwable {
+
+    ArrayList<Node<int[]>> children = this.node.getChildren();
+
+    assertEquals(1, children.size());
+
+    assertEquals(this.node, this.childNode.getParent());
+
+    int childX = children.get(0).getData()[0];
+    int childY = children.get(0).getData()[1];
+
+    assertEquals(this.childX, childX);
+    assertEquals(this.childY, childY);
   }
 
 }
